291e377ad8a049aab714eb4723bb8701b2cf5383
Atharva <atharvajoshi202@gmail.com>
2022-11-24 23:35:06 +0530

    High Score functionality added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const handOptions = {
-  "rock": "/assets/Rock.png",
-  "paper": "/assets/Paper.png",
-  "scissors": "/assets/Scissors.png"
-}
+  rock: "/assets/Rock.png",
+  paper: "/assets/Paper.png",
+  scissors: "/assets/Scissors.png",
+};
 let heighScore = 0;
 let SCORE = 0;
 
@@ -20,13 +20,13 @@ const pickUserHand = (hand) => {
 };
 
 const pickComputerHand = (hand) => {
-    let hands = ["rock", "paper", "scissors"];
-    let cpHand = hands[Math.floor(Math.random() * hands.length)];
-    
-    // set computer image 
-    document.getElementById("computerPickImage").src = handOptions[cpHand]
-    
-    referee(hand, cpHand);
+  let hands = ["rock", "paper", "scissors"];
+  let cpHand = hands[Math.floor(Math.random() * hands.length)];
+
+  // set computer image
+  document.getElementById("computerPickImage").src = handOptions[cpHand];
+
+  referee(hand, cpHand);
 };
 
 const referee = (userHand, cpHand) => {
@@ -71,24 +71,53 @@ const restartGame = () => {
 
   let hands = document.querySelector(".hands");
   hands.style.display = "flex";
-}
+};
 
 const setDecision = (decision) => {
   document.querySelector(".decision h1").innerText = decision;
+};
+
+let h = localStorage.getItem("High Score");
+
+console.log(h);
+
+if (h == null) {
+  localStorage.setItem("High Score", heighScore);
+  heighScore = localStorage.getItem("High Score");
 }
+heighScore = localStorage.getItem("High Score");
+document.querySelector("#hscore").innerText = `High Score: ${heighScore}`;
 
 const setScore = (newScore) => {
   SCORE = newScore;
   document.querySelector(".score h1").innerText = newScore;
-  console.log(heighScore);
-  if(SCORE > heighScore)
-  {
-  heighScore = SCORE;
-  console.log("Previous heigh score is ");
-  console.log(SCORE-1);
-  console.log("You made heigh score");
-  console.log(heighScore);
-}
+  // console.log(heighScore);
+
+  if (SCORE > heighScore) {
+    heighScore = SCORE;
+    console.log("You made heigh score");
+    console.log(heighScore);
+    document.querySelector("#hscore").innerText = `High Score: ${heighScore}`;
+    localStorage.setItem("High Score", heighScore);
+  }
+};
+
+let name = localStorage.getItem("Name");
+console.log(name);
+
+if (name) {
+  document.querySelector("#name").innerText = `Welcome ${name}`;
+} else {
+  name = prompt("Enter your name");
+  localStorage.setItem("Name", name);
+  document.querySelector("#name").innerText = `Welcome ${name}`;
 }
 
+// window.addEventListener("storage", (e) => {
+//   if (e.key === "High Score") {
+//     // changeTheme(e.newValue);
+//     localStorage.setItem("High Score", e.newValue);
+//   }
 
+//   console.log(e);
+// });
